7b853e80e0e850e16c9423af126ed84f2d95e2e6
devshankha <45039485+devshankha@users.noreply.github.com>
2019-05-04 22:05:12 +0530

    Add files via upload

---
+++ stringtest.py
@@ -0,0 +1,26 @@
+str =r"This is g\treat"
+print(str)
+s ="How you"
+print(len(s))
+
+name = u'\u0915\u094b\u0930 \u092a\u0948\u0925\u0964\u0928'
+print(name)
+
+s1 = "David"
+i = 0;
+while (i < len(s1)):
+    print(s1[i],end='  ')
+    i=i+1
+
+s2 = "How are you"
+print(s2[0:9:2])
+
+print(s2[::2])
+
+s7 ="Kaju"
+s7 = s7*3
+print(s7)
+
+if "aj" in s7:
+    print("It is there")
+
