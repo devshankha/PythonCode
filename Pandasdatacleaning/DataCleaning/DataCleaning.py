9033da7181c1cf85904a5a1d1e35a788158c0a46
devshankha <45039485+devshankha@users.noreply.github.com>
2021-06-10 19:36:26 +0530

    Update DataCleaning.py

--- Pandasdatacleaning/DataCleaning/DataCleaning.py
+++ Pandasdatacleaning/DataCleaning/DataCleaning.py
@@ -32,3 +32,18 @@ inp0['Android Ver'].mode()
 
 #Imputation, replace missing values in android column with the mode
 inp0['Android Ver'] =inp0['Android Ver'].fillna(inp0['Android Ver'].mode()[0])
+
+# this will help you check the datatypes of the different columns
+inp0.dtypes
+
+#check  values in the price column
+inp0.Price.value_counts()
+
+#remove the dollar symbol in price so that it becomes int or float
+#this does-not seem to be working
+inp0.Price =inp0.Price.apply(lambda x: 0 if x==0  else  float(x[1:]))
+
+#Write the function to make the changes
+#this also doesnt seem to work
+#basically reviews is object change it into int
+inp0.Reviews = inp0.Reviews.astype("int32")
